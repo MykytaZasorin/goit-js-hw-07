e840813b11575e3f2c3ff10701fd8e8ec176d875
Mykyta_zn <mykytazasorin@gmail.com>
2025-10-29 17:30:00 +0200

    fin 1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const totalCategories = document.querySelectorAll('#categories .item');
+
+console.log('Number of categories: ', totalCategories.length);
+
+for (const category of totalCategories) {
+  const categoryName = category.querySelector('h2').textContent;
+  const totalElements = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${totalElements}`);
+}
+
+
+
